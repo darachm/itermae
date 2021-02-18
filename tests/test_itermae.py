bb181f73040294978bf563ff7fe99d4fcc8f2c96
darachm <nope>
2021-02-18 10:47:58 -0800

    okay, folding all the small seqholder tests together, into one big test with a lot of results coded in a zipped array and a lot of assets that they match

--- tests/test_itermae.py
+++ tests/test_itermae.py
@@ -66,46 +66,30 @@ def test_groupstats_flatten(groupstats):
 def test_seqholder_verbosity(fastqfile):
     for i in fastqfile:
         seqholder = itermae.SeqHolder(i,verbosity=3)
+        # Is the verbosity right? (this is a gimme)
         assert seqholder.verbosity == 3
 
 # Test SeqHolder dummyspacer is right
 def test_seqholder_dummy(fastqfile):
     for i in fastqfile:
         seqholder = itermae.SeqHolder(i,verbosity=3)
+        # Is the sequence X?
         assert seqholder.seqs['dummyspacer'].seq == 'X'
+        # Is the number we just put there 40?
         assert seqholder.seqs['dummyspacer'].letter_annotations['phred_quality'] == [40] 
 
-# Test that SeqHolder can match with apply_operation
-def test_seqholder_match(fastqfile):
-    for seq, pos_pass, qual_pass, seq_pass in zip(fastqfile,
-            [ [i == 1] for i in [1,1,0,1,0,0,1,1,1] ],
-            [ [i == 1] for i in [0,0,0,1,0,0,1,1,0] ],
-            [ [i == 1] for i in [1,0,0,0,0,0,0,1,0] ]
-            ):
-        seqholder = itermae.SeqHolder(seq,verbosity=3)
-        seqholder.apply_operation('a','input',
-            regex.compile("(?P<sample>[ATCG]{5})(?P<fixed1>GTCCACGAGGTC){e<=2}(?P<rest>TCT.*){e<=1}",
-                regex.BESTMATCH) )
-        seqholder.apply_operation('b','rest',
-            regex.compile("(?P<tag>TCT){e<=1}(?P<strain>[ATCG]{10,26})(CGTACGCTGC){e<=2}",
-                regex.BESTMATCH) )
-        seqholder.apply_operation('c','rest',
-            regex.compile("(?P<fixed2>CGTACGCTGCAGGTC)(?<UMItail>GAC[ATCG]G[ATCG]A[ATCG]G[ATCG]G[ATCG]G[ATCG]GAT){s<=2}",
-                regex.BESTMATCH) )
-        assert pos_pass == seqholder.apply_filters(['sample.length == 5 and rest.start >= 15']) 
-        assert qual_pass == seqholder.apply_filters(['statistics.mean(fixed1.quality) >= 33.5'])
-        assert seq_pass == seqholder.apply_filters(['sample.seq == "TTCAC" or sample.seq == "AGGAG"'])
-
-
-# Test that filters shortread fastq
-
+# Test that SeqHolder can apply_operation, then since we're there testing
+# that it finds the right groups for each seq, and passes or fails filters 
+# appropriately.
+# Shortread FASTQ input
 def test_seqholder_match_filter(fastqfile):
-    for seq, pos_pass, qual_pass, seq_pass, sequences_found in zip(fastqfile,
+    for seq, pos_pass, qual_pass, seq_pass, sequences_found, \
+        seq_targets, report_targets \
+        in zip(fastqfile,
             [ [i == 1] for i in [1,1,0,1,0,0,1,1,1] ],
             [ [i == 1] for i in [0,0,0,1,0,0,1,1,0] ],
             [ [i == 1] for i in [1,0,0,0,0,0,0,1,0] ],
-            [
-                set(['dummyspacer','input','sample','fixed1','rest','tag','strain']),
+            [   set(['dummyspacer','input','sample','fixed1','rest','tag','strain']),
                 set(['dummyspacer','input','sample','fixed1','rest','tag','strain','fixed2','UMItail']),
                 set(['dummyspacer','input']),
                 set(['dummyspacer','input','sample','fixed1','rest','tag','strain','fixed2','UMItail']),
@@ -114,9 +98,31 @@ def test_seqholder_match_filter(fastqfile):
                 set(['dummyspacer','input','sample','fixed1','rest','tag','strain','fixed2','UMItail']),
                 set(['dummyspacer','input','sample','fixed1','rest','tag','strain','fixed2','UMItail']),
                 set(['dummyspacer','input','sample','fixed1','rest','tag','strain','fixed2','UMItail']),
+            ],
+            [   ('ExampleToyReads:1:exampleFlowCell:1:10000:10000:10000_TTCAC','TCAGTCGTAGCAGTTCGATG'),
+                ('ExampleToyReads:1:exampleFlowCell:1:10000:10001:10001_GCTTC', 'TGGCAGACACACGCTACA'),
+                (None,None),
+                ('ExampleToyReads:1:exampleFlowCell:1:10000:10003:10003_CTACT', 'GATGCACTGCGTTCCATGTT'),
+                (None,None),
+                (None,None),
+                ('ExampleToyReads:1:exampleFlowCell:1:10000:10006:10006_TCGGC', 'ATTCTGAGCGGTGCCATAGT'),
+                ('ExampleToyReads:1:exampleFlowCell:1:10000:10007:10007_AGGAG', 'ATAAGTTAGACAGGTCAGC'),
+                ('ExampleToyReads:1:exampleFlowCell:1:10000:10008:10008_ACGTA', 'CACACGCACGAATTTGCATA')
+                ],
+            [   '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10000:10000","TTCACGTCCTCGAGGTCTCTTCAGTCGTAGCAGTTCGATGCGTACGCTACAGGTCGACGGTAAGAGAGGGATGTG","TCAGTCGTAGCAGTTCGATG","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20"',
+                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10001:10001","GCTTCGTCCTCGAGGTCTATTGGCAGACACACGCTACACGTACGCTGCAGGTCGAGGGCACGCGAGAGATGTGTG","TGGCAGACACACGCTACA","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_21_18-fixed2_21_36_15-UMItail_36_53_17"',
+                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10002:10002","CCCGGCGTTCGGGGAAGGACGTCAATAGTCACACAGTCCTTGACGGTATAATAACCACCATCATGGCGACCATCC","TGGCAGACACACGCTACA","FilterResults",""',
+                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10003:10003","CTACTGTCCACGAGGTCTCTGATGCACTGCGTTCCATGTTCGTACGCTGCAGGTCGACGGAAGGAGCGCGATGTG","GATGCACTGCGTTCCATGTT","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"',
+                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10004:10004","AAATTAGGGTCAACGCTACCTGTAGGAAGTGTCCGCATAAAGTGCACCGCATGGAAATGAAGACGGCCATTAGCT","GATGCACTGCGTTCCATGTT","FilterResults",""',
+                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10005:10005","CCCGGCGTTCGGGGAAGGACGTCAATAGTCACACAGTCCTTGACGGTATAATAACCACCATCATGGCGACCATCC","GATGCACTGCGTTCCATGTT","FilterResults",""',
+                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10006:10006","TCGGCGTCCTCGAGGTCTCTATTCTGAGCGGTGCCATAGTCGTACGCTGCAGGTCGACCGAAGGTGGGAGATGTG","ATTCTGAGCGGTGCCATAGT","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"',
+                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10007:10007","AGGAGGTCCTCGAGGTCTCTATAAGTTAGACAGGTCAGCCGTACGCTGCAGGTCGACAGCTGGCGCGCGATGTGA","ATAAGTTAGACAGGTCAGC","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_22_19-fixed2_22_37_15-UMItail_37_54_17"',
+                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10008:10008","ACGTAGTCCACGAGGTCTCTCACACGCACGAATTTGCATACGTACGCTGCAGGTCGACTGGAAGGGCGGGATGTG","CACACGCACGAATTTGCATA","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"'
             ]
             ):
+        # Read in the sequence to the holder
         seqholder = itermae.SeqHolder(seq,verbosity=0)
+        # Apply operations
         seqholder.apply_operation('a','input',
             regex.compile("(?P<sample>[ATCG]{5})(?P<fixed1>GTCCACGAGGTC){e<=2}(?P<rest>TCT.*){e<=1}",
                 regex.BESTMATCH) )
@@ -134,55 +140,20 @@ def test_seqholder_match_filter(fastqfile):
         assert qual_pass == seqholder.apply_filters(['statistics.mean(fixed1.quality) >= 33.5'])
         # Does it pass a specific sequence filter?
         assert seq_pass == seqholder.apply_filters(['sample.seq == "TTCAC" or sample.seq == "AGGAG"'])
-
-# Test shortread fastq
-
-def test_seqholder_outputs(fastqfile):
-    for seq, seq_targets, filter_targets in zip(fastqfile,
-            [ 
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10000:10000_TTCAC','TCAGTCGTAGCAGTTCGATG'),
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10001:10001_GCTTC', 'TGGCAGACACACGCTACA'),
-                (None,None),
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10003:10003_CTACT', 'GATGCACTGCGTTCCATGTT'),
-                (None,None),
-                (None,None),
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10006:10006_TCGGC', 'ATTCTGAGCGGTGCCATAGT'),
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10007:10007_AGGAG', 'ATAAGTTAGACAGGTCAGC'),
-                ('ExampleToyReads:1:exampleFlowCell:1:10000:10008:10008_ACGTA', 'CACACGCACGAATTTGCATA')
-                ],
-                            [
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10000:10000","TTCACGTCCTCGAGGTCTCTTCAGTCGTAGCAGTTCGATGCGTACGCTACAGGTCGACGGTAAGAGAGGGATGTG","TCAGTCGTAGCAGTTCGATG","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20"',
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10001:10001","GCTTCGTCCTCGAGGTCTATTGGCAGACACACGCTACACGTACGCTGCAGGTCGAGGGCACGCGAGAGATGTGTG","TGGCAGACACACGCTACA","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_21_18-fixed2_21_36_15-UMItail_36_53_17"',
-                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10002:10002","CCCGGCGTTCGGGGAAGGACGTCAATAGTCACACAGTCCTTGACGGTATAATAACCACCATCATGGCGACCATCC","TGGCAGACACACGCTACA","FilterResults",""',
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10003:10003","CTACTGTCCACGAGGTCTCTGATGCACTGCGTTCCATGTTCGTACGCTGCAGGTCGACGGAAGGAGCGCGATGTG","GATGCACTGCGTTCCATGTT","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"',
-                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10004:10004","AAATTAGGGTCAACGCTACCTGTAGGAAGTGTCCGCATAAAGTGCACCGCATGGAAATGAAGACGGCCATTAGCT","GATGCACTGCGTTCCATGTT","FilterResults",""',
-                '"fail_to_form","ExampleToyReads:1:exampleFlowCell:1:10000:10005:10005","CCCGGCGTTCGGGGAAGGACGTCAATAGTCACACAGTCCTTGACGGTATAATAACCACCATCATGGCGACCATCC","GATGCACTGCGTTCCATGTT","FilterResults",""',
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10006:10006","TCGGCGTCCTCGAGGTCTCTATTCTGAGCGGTGCCATAGTCGTACGCTGCAGGTCGACCGAAGGTGGGAGATGTG","ATTCTGAGCGGTGCCATAGT","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"',
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10007:10007","AGGAGGTCCTCGAGGTCTCTATAAGTTAGACAGGTCAGCCGTACGCTGCAGGTCGACAGCTGGCGCGCGATGTGA","ATAAGTTAGACAGGTCAGC","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_22_19-fixed2_22_37_15-UMItail_37_54_17"',
-                '"pass","ExampleToyReads:1:exampleFlowCell:1:10000:10008:10008","ACGTAGTCCACGAGGTCTCTCACACGCACGAATTTGCATACGTACGCTGCAGGTCGACTGGAAGGGCGGGATGTG","CACACGCACGAATTTGCATA","FilterResults","sample_0_5_5-fixed1_5_17_12-rest_17_75_58-tag_0_3_3-strain_3_23_20-fixed2_23_38_15-UMItail_38_55_17"'
-                ],
-            ):
-        seqholder = itermae.SeqHolder(seq,verbosity=3)
-        seqholder.apply_operation('a','input',
-            regex.compile("(?P<sample>[ATCG]{5})(?P<fixed1>GTCCACGAGGTC){e<=2}(?P<rest>TCT.*){e<=1}",
-                regex.BESTMATCH) )
-        seqholder.apply_operation('b','rest',
-            regex.compile("(?P<tag>TCT){e<=1}(?P<strain>[ATCG]{10,26})(CGTACGCTGC){e<=2}",
-                regex.BESTMATCH) )
-        seqholder.apply_operation('c','rest',
-            regex.compile("(?P<fixed2>CGTACGCTGCAGGTC)(?<UMItail>GAC[ATCG]G[ATCG]A[ATCG]G[ATCG]G[ATCG]G[ATCG]GAT){s<=2}",
-                regex.BESTMATCH) )
+        # Then build outputs
         (this_output_id, this_output_seq) = (None,None)
         try:
             this_output = seqholder.build_output(
                     "input.id+'_'+sample.seq",
                     "strain" ) 
             (this_output_id, this_output_seq) = (this_output.id,this_output.seq)
-            this_result = seqholder.format_report("pass",this_output.seq,"FilterResults")
+            this_report = seqholder.format_report("pass",this_output.seq,"FilterResults")
         except:
-            this_result = seqholder.format_report("fail_to_form",this_output.seq,"FilterResults")
+            this_report = seqholder.format_report("fail_to_form",this_output.seq,"FilterResults")
+        # Are the right outputs constructed?
         assert seq_targets == ( this_output_id, this_output_seq ) 
-        assert filter_targets == this_result
+        # Are the right filter reports constructed?
+        assert report_targets == this_report
 
 #
 # Full Tests
