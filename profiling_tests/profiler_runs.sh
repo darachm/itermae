35f5d6f7b694070631139c2aad278156e84a6e35
darachm <nope>
2021-02-20 15:30:40 -0800

    fixing path for profiling

--- profiling_tests/profiler_runs.sh
+++ profiling_tests/profiler_runs.sh
@@ -4,7 +4,7 @@
 # Single core of course
 
 # First, simple one operation one output
-seq 50 | xargs -I{} cat example-data/barseq.fastq \
+seq 50 | xargs -I{} cat itermae/data/barseq.fastq \
     | python3 -m cProfile -o profiling_tests/simple_opOne_outOne.profile \
         bin/itermae \
             -o "input > (?P<sampleIndex>[ATCGN]{5,5})(?P<rest>(GTCCACGAG){e<=1}[ATCGN]*)" \
@@ -13,7 +13,7 @@ seq 50 | xargs -I{} cat example-data/barseq.fastq \
     > profiling_tests/simple_opOne_outOne.fasta
 
 # Long complex operation, one output
-seq 50 | xargs -I{} cat example-data/barseq.fastq \
+seq 50 | xargs -I{} cat itermae/data/barseq.fastq \
     | python3 -m cProfile -o profiling_tests/long_opOne_outOne.profile \
         bin/itermae \
             -o "input > (?P<sampleIndex>[ATCGN]{5,5})(?P<upPrime>GTCCTCGAGGTCTCT){e<=1}(?P<barcode>[ATCGN]{18,22})(?P<downPrime>CGTACGCTG){e<=1}" \
@@ -22,7 +22,7 @@ seq 50 | xargs -I{} cat example-data/barseq.fastq \
     > profiling_tests/long_opOne_outOne.fasta
 
 # Two simpler operations, one output
-seq 50 | xargs -I{} cat example-data/barseq.fastq \
+seq 50 | xargs -I{} cat itermae/data/barseq.fastq \
     | python3 -m cProfile -o profiling_tests/opTwo_outOne.profile \
         bin/itermae \
             -o "input > (?P<sampleIndex>[ATCGN]{5,5})(?P<rest>(GTCCTCGAGGTCTCT){e<=1}[ATCGN]*)" \
@@ -32,7 +32,7 @@ seq 50 | xargs -I{} cat example-data/barseq.fastq \
     > profiling_tests/opTwo_outOne.fasta
 
 # Two simpler operations, two outputs
-seq 50 | xargs -I{} cat example-data/barseq.fastq \
+seq 50 | xargs -I{} cat itermae/data/barseq.fastq \
     | python3 -m cProfile -o profiling_tests/opTwo_outTwo.profile \
         bin/itermae \
             -o "input > (?P<sampleIndex>[ATCGN]{5,5})(?P<rest>(GTCCTCGAGGTCTCT){e<=1}[ATCGN]*)" \
